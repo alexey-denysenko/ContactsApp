ecf85e65a321c21d14b3d8167b31ca899da27a2b
oleksii <likezsmj@gmail.com>
2019-03-24 14:16:22 +0200

    Add timber

--- app/src/main/java/com/example/contactsapplication/ContactsApplication.java
+++ app/src/main/java/com/example/contactsapplication/ContactsApplication.java
@@ -3,8 +3,16 @@ package com.example.contactsapplication;
 
 import dagger.android.AndroidInjector;
 import dagger.android.support.DaggerApplication;
+import timber.log.Timber;
 
 public class ContactsApplication extends DaggerApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Timber.plant(new Timber.DebugTree());
+    }
+
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
         return DaggerContactsAppComponent.builder()
