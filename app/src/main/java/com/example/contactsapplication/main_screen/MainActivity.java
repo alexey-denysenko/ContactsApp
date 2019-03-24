28ff06eae399ddfc92000c6c0cbab4a2de3d954e
oleksii <likezsmj@gmail.com>
2019-03-24 14:16:40 +0200

    Add recyclerView initialization

--- app/src/main/java/com/example/contactsapplication/main_screen/MainActivity.java
+++ app/src/main/java/com/example/contactsapplication/main_screen/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.contactsapplication.main_screen;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.contactsapplication.R;
+import com.example.contactsapplication.main_screen.list.ContactsAdapter;
 
 import javax.inject.Inject;
 
@@ -15,10 +18,22 @@ public class MainActivity extends DaggerAppCompatActivity {
     @Inject
     ViewModelProvider.Factory factory;
 
+    private RecyclerView recyclerView;
+    private ContactsAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ViewModelProviders.of(this, factory).get(MainActivityViewModel.class);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        adapter = new ContactsAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
